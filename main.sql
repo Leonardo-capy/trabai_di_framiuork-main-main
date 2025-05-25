541d13413d1a3ab975922a0721a29b8d5d6fc301
Leonardo-capy <leonardo.santos17@estudante.ifms.edu.br>
2025-05-25 10:05:25 -0500

    Add files via upload

--- main.sql
+++ main.sql
@@ -3,18 +3,17 @@ CREATE DATABASE trabalho_catalogo;
 USE trabalho_catalogo;
 
 CREATE TABLE produto (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255),
     preco DECIMAL(10,2),
-    vendedor VARCHAR(255),
-    PRIMARY KEY (id)
+    vendedor VARCHAR(255)
 );
 
-CREATE TABLE usuario (
-	id_usuario INT PRIMARY KEY NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL,
-    CHECK (CHAR_LENGTH(senha)  >=8 ),
-    email VARCHAR(255),
-    CHECK (email LIKE '%@%')
+CREATE TABLE pessoa (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL, 
+    email VARCHAR(100) NOT NULL, 
+    senha VARCHAR(100) NOT NULL, 
+    telefone VARCHAR(20) NOT NULL, 
+    genero VARCHAR(20) NOT NULL
 );
